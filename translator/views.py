8c1db70acab3fa6a4a4a0aa631db6199ef3fc317
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2020-03-27 21:46:28 -0700

    russian translator working/alphabet added

--- translator/views.py
+++ translator/views.py
@@ -5,8 +5,46 @@ from googletrans import Translator
 # Create your views here.
 
 def index(request):
-    translator = Translator()
-    translations = translator.translate(['Hello world'], dest='ru')
 
-    context = {'translations':translations}
+    russAlph = {
+        'Аа':'a',
+        'Бб':'b',
+        'Вв':'v',
+        'Гг':'g',
+        'Дд':'d',
+        'Ее':'e, ye',
+        'Ёё':'yo, jo, ë',
+        'Жж':'zh, ž',
+        'Зз':'z',
+        'Ии':'i',
+        'Йй':'y, i, j',
+        'Кк':'k',
+        'Лл':'l',
+        'Мм':'m',
+        'Нн':'n',
+        'Оо':'o',
+        'Пп':'p',
+        'Рр':'r',
+        'Сс':'s',
+        'Тт':'t',
+        'Уу':'u',
+        'Фф':'f',
+        'Хх':'kh, h, x',
+        'Цц':'ts, c',
+        'Чч':'ch, č',
+        'Шш':'sh, š',
+        'Щщ':'shch, sch, šč',
+        'Ъъ':'ʺ',
+        'Ыы':'y',
+        'Ьь':'"',
+        'Ээ':'e, è',
+        'Юю':'yu, ju',
+        'Яя':'ya, ja',
+    }
+
+    
+    translator = Translator()
+    translations = translator.translate([''], dest='ru')
+
+    context = {'translations':translations, 'alphabet':russAlph}
     return render(request, 'dash/main.html', context)
