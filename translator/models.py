2351a780c92fd20a09421fdbbedd29813ed2b094
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2020-03-29 12:35:22 -0700

    added alphabet/number models

--- translator/models.py
+++ translator/models.py
@@ -2,4 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+class Alphabet(models.Model):
+    letter = models.name = models.CharField(max_length=10)
+    name = models.name = models.CharField(max_length=100)
+    transliteration = models.name = models.CharField(max_length=100)
+    equivalent = models.name = models.CharField(max_length=200)
+    examples = models.name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.letter
+
+    
+class Number(models.Model): 
+    name = models.name = models.CharField(max_length=100)
+    number = models.name = models.CharField(max_length=10000)
+    transliteration = models.name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- translator/views.py
+++ translator/views.py
@@ -2,49 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from googletrans import Translator
 
+from .models import *
+
 # Create your views here.
 
 def index(request):
 
-    russAlph = {
-        'Аа':'a',
-        'Бб':'b',
-        'Вв':'v',
-        'Гг':'g',
-        'Дд':'d',
-        'Ее':'e, ye',
-        'Ёё':'yo, jo, ë',
-        'Жж':'zh, ž',
-        'Зз':'z',
-        'Ии':'i',
-        'Йй':'y, i, j',
-        'Кк':'k',
-        'Лл':'l',
-        'Мм':'m',
-        'Нн':'n',
-        'Оо':'o',
-        'Пп':'p',
-        'Рр':'r',
-        'Сс':'s',
-        'Тт':'t',
-        'Уу':'u',
-        'Фф':'f',
-        'Хх':'kh, h, x',
-        'Цц':'ts, c',
-        'Чч':'ch, č',
-        'Шш':'sh, š',
-        'Щщ':'shch, sch, šč',
-        'Ъъ':'ʺ',
-        'Ыы':'y',
-        'Ьь':'"',
-        'Ээ':'e, è',
-        'Юю':'yu, ju',
-        'Яя':'ya, ja',
-    }
+    alphabet = Alphabet.objects.all()
+    numbers = Number.objects.all()
 
     
     translator = Translator()
     translations = translator.translate([''], dest='ru')
 
-    context = {'translations':translations, 'alphabet':russAlph}
-    return render(request, 'dash/main.html', context)
+    context = {'translations':translations, 'alphabet':alphabet, 'numbers':numbers}
+    return render(request, 'dash/index.html', context)
